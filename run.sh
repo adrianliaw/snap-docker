00d81bb31c430b3e33bc5402ea7201a62a2bf695
adrianliaw <adrianliaw2000@gmail.com>
2017-03-21 21:31:02 +0800

    Add longer timeout, modify plugins url, use plugin built for linux/amd64

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 /usr/local/bin/init_snap
 
 # Start Snap daemon
-snapteld -t ${SNAP_TRUST_LEVEL} -l ${SNAP_LOG_LEVEL} -o '' &
+snapteld -t ${SNAP_TRUST_LEVEL} -l ${SNAP_LOG_LEVEL} --plugin-load-timeout 30 -o '' &
 SNAP_PID=$(pidof snapteld)
 
 echo
@@ -11,8 +11,8 @@ echo "[Process $SNAP_PID] Load plugins: docker, meminfo, pyustil, and influxdb."
 echo
 
 curl -sfL "http://snap.ci.snap-telemetry.io/plugins/snap-plugin-collector-meminfo/latest/linux/x86_64/snap-plugin-collector-meminfo" -o snap-plugin-collector-meminfo
-curl -sfL "https://github.com/intelsdi-x/snap-plugin-publisher-influxdb/releases/download/16/snap-plugin-publisher-influxdb_linux_x86_64" -o snap-plugin-publisher-influxdb
-curl -sfL "https://github.com/adrianliaw/snap-plugin-collector-docker/releases/download/5-name/snap-plugin-collector-docker_linux_x86_64" -o snap-plugin-collector-docker
+curl -sfL "https://github.com/intelsdi-x/snap-plugin-publisher-influxdb/releases/download/21/snap-plugin-publisher-influxdb_linux_x86_64" -o snap-plugin-publisher-influxdb
+curl -sfL "https://github.com/intelsdi-x/snap-plugin-collector-docker/releases/download/7/snap-plugin-collector-docker_linux_x86_64" -o snap-plugin-collector-docker
 curl -sfL "https://github.com/intelsdi-x/snap-plugin-collector-disk/releases/download/4/snap-plugin-collector-disk_linux_x86_64" -o snap-plugin-collector-disk
 curl -sfL "https://github.com/intelsdi-x/snap-plugin-collector-psutil/releases/download/8/snap-plugin-collector-psutil_linux_x86_64" -o snap-plugin-collector-psutil
 
